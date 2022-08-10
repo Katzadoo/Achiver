01c601470ec6fec9b3c9b14961d151343f9bb0dd
Katz <Dalitsocae@gmail.com>
2022-08-10 23:11:12 +0200

    Add boiler plate, test zip file and unzipped file

---
+++ main.py
@@ -0,0 +1,9 @@
+import zipfile
+import os
+
+print(os.listdir())
+#sublimes terminal doesnt support input, its just an out. Commenting for now.
+data = input('Choose which .zip file you want to extract(Case sensitive): ')
+
+with zipfile.ZipFile(data, 'r') as zipobj:
+	print(zipobj.namelist())
