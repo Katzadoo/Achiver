6973f675fddd2262d8d2a5c876c37d6f22d12850
KatzDev <Dalitsocae@gmail.com>
2022-08-12 19:30:58 +0200

    wrap statement in try and except

--- main.py
+++ main.py
@@ -2,8 +2,12 @@ import zipfile
 import os
 
 print(os.listdir())
-#sublimes terminal doesnt support input, its just an out. Commenting for now.
 data = input('Choose which .zip file you want to extract(Case sensitive): ')
 
-with zipfile.ZipFile(data, 'r') as zipobj:
-	print('List of files: ' + str(zipobj.namelist()))
+try:
+	with zipfile.ZipFile(data, 'r') as zipobj:
+		print('List of files: ' + str(zipobj.namelist()))
+
+except:
+	print('please use a valid zip file type.')
+
