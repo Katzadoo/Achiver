46f8582862ecb78b6aeff645066f4b05c71dfa78
KatzDev <Dalitsocae@gmail.com>
2022-08-12 19:39:45 +0200

    It zips to extracted now, wrapped in try and except statement

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ data = input('Choose which .zip file you want to extract(Case sensitive): ')
 
 try:
 	with zipfile.ZipFile(data, 'r') as zipobj:
-		print('List of files: ' + str(zipobj.namelist()))
+		zipobj.extractall("extracted/")
 
 except:
 	print('please use a valid zip file type.')
